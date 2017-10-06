7704d222c43008d8e0a7910f4f839df0ee0013a4
iluvater <ture.kuesener@gmx.de>
2017-10-06 20:46:03 +0200

    Aufgabe 3

--- WebTest/src/ServletTest.java
+++ WebTest/src/ServletTest.java
@@ -23,11 +23,18 @@ public class ServletTest extends HttpServlet {
     }
 
 	/**
+	 * 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String b[] = request.getParameterValues("user");
+		
+		response.getWriter().println(b[0]);
+		
+		
+		
 	}
 
 	/**
